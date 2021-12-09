adc929ec1a94f0371475a6d833e0d6bb45ae7eca
Christian Rua <christian.rua@gmail.com>
2021-12-09 13:11:35 -0500

    adding comments lesson

--- first_script.sh
+++ first_script.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+# Author: Christian Rua
+# Date Created: 2021/12/09
+# Last Modified: 2021/12/09
+
+# Description
+# Prints "This is my first script" to the terminal
+
+# Usage 
+# our_first_script
+
+
 echo "this is my first script"
 exit 0
